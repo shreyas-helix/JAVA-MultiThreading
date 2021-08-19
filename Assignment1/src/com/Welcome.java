f7d75da4cfec13a2cb3e15093aba4a974e4b76b9
shreyas-helix <shreyaskv94@gmail.com>
2021-08-19 11:54:19 +0530

    Bootstrap
    Welcome

---
+++ Assignment1/src/com/Welcome.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Shreyas Workspace!!");
+	}
+
+}
